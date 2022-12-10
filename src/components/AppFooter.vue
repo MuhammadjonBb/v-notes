67a59c6074bd6ef12c5da97d39304ebeac34e5f4
mukhammadzhon_boborakhimov <muhammadzonboborakhimov7@gmail.com>
2022-12-10 17:03:47 +0500

    initial

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+footer
+  .container
+    ul.navbar-list
+      li.navbar-item Home
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+header.navbar
+  .container
+    .navbar-content
+      a.navbar-logo(href="/") logo
+      ul.navbar-list
+        li.navbar-item Home
+</template>
