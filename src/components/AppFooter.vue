3a1ecdf379e2b8c23084c8a6b7f5d314a4fc74ce
mukhammadzhon_boborakhimov <muhammadzonboborakhimov7@gmail.com>
2022-12-11 11:48:51 +0500

    add navbar links rendering from config.js

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
 footer
   .container
-    ul.navbar-list
-      li.navbar-item Home
+      ul.navbar-list
+        li.navbar-item(v-for="link in links" :key="link.alias")
+          router-link.navbar-link(:to="link.url") {{ link.title }}
 </template>
+
+<script>
+/* eslint-disable quotes */
+import { links } from "@/_config";
+
+export default {
+  data() {
+    return {
+      links,
+    };
+  },
+};
+</script>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,5 +4,19 @@ header.navbar
     .navbar-content
       a.navbar-logo(href="/") logo
       ul.navbar-list
-        li.navbar-item Home
+        li.navbar-item(v-for="link in links" :key="link.alias")
+          router-link.navbar-link(:to="link.url") {{ link.title }}
 </template>
+
+<script>
+/* eslint-disable quotes */
+import { links } from "@/_config";
+
+export default {
+  data() {
+    return {
+      links,
+    };
+  },
+};
+</script>
